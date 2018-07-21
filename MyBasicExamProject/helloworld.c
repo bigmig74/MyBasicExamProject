939f2bee6888f41e38d3ba2421bb2117ef082da3
Miguel Chavarria <miguel_chavarria@hotmail.com>
2018-07-21 17:53:02 -0500

    Second Commit still practicing

---
+++ MyBasicExamProject/helloworld.c
@@ -0,0 +1,14 @@
+/*
+ * helloworld.c
+ *
+ *  Created on: Jul 21, 2018
+ *      Author: miguel
+ */
+
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+	printf("Hello World!\n");
+	return 0;
+}
